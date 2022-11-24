e00b1ce450931b8eec9f4176322f380fad67c89c
thedata-ADDICT <103414427+thedata-ADDICT@users.noreply.github.com>
2022-11-24 11:16:02 +0530

    Directory Changed

--- Teams Power Users.sql
+++
@@ -1,8 +0,0 @@
-SELECT sender_id,
-count(1) as message_count
-FROM messages
-where date_part('month',sent_date)=08
-and date_part('year',sent_date)=2022
-GROUP BY sender_id
-order by message_count DESC
-limit 2;
